2d5069e3b6ee8e3fef4b4d2da7517937b02d57c0
VINAY KUMAR <vinay21031998@gmail.com>
2022-10-10 18:39:57 +0530

    LRU cache design

---
+++ src/main/java/com/cache/design/DesignApplication.java
@@ -0,0 +1,13 @@
+package com.cache.design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignApplication.class, args);
+	}
+
+}
